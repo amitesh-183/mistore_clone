706c12866ad52125064fc2368fc0a4ac76e2f880
B.Amitesh <bamitesh520@gmail.com>
2022-06-01 17:45:48 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/nav.css";
-import { logo } from "../data/data.json";
+import logo from "../data/data.json";
 import { Link } from "react-router-dom";
 // import NavOptions from "./NavOptions";
 
